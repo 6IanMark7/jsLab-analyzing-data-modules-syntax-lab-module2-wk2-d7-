d02db397d0e07ee0b5035427d6ee45e4b4c20616
Ian <6markian7@gmail.com>
2026-04-06 15:20:27 +0300

    completed Module Rest-Spread Syntax

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 
-
-
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  const mergeArgs = args.flat();
+  
+  combinedObject.users=[...mergeArgs] ;
+  
+  const todays = new Date();
+  const year = todays.getFullYear();
+  const month = todays.getMonth() + 1;
+  const date = todays.getDate();
+  
+  combinedObject.merge_date =`${month}/${date}/${year}` ;
+  return combinedObject;
+  
+}
+console.log(combineUsers("UserName1","UserName2","UserName3","UserName4","UserName5"));
+
